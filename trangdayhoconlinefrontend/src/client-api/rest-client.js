ba01ea17ba6dbd9a833b425181e28ae880c7b4fb
Tran Van Hung <hungtranvan1110@gmail.com>
2024-11-11 13:05:35 +0700

    Tạo và quản lý khóa học,
    + tạo khóa học
    + chỉnh sửa khóa học
    +xem khóa học

--- trangdayhoconlinefrontend/src/client-api/rest-client.js
+++ trangdayhoconlinefrontend/src/client-api/rest-client.js
@@ -13,27 +13,6 @@ class RestClient {
         this.baseUrl = url;
     }
 
-    // Tạm thời không sử dụng token trong quá trình xác thực
-    async authentication(strategy, email, password) {
-        try {
-            const response = await fetch(`${this.baseUrl}/auth/${strategy}`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ email, password })
-            });
-            const data = await response.json();
-            return data;
-        } catch (error) {
-            console.error('Authentication failed:', error);
-        }
-    }
-
-    async reAuthentication() {
-        console.warn('Re-authentication is not implemented without a token.');
-    }
-
     async find(query = {}) {
         try {
             const url = new URL(`${this.baseUrl}/${this.path}`);
@@ -44,14 +23,14 @@ class RestClient {
                 headers: {
                     'Content-Type': 'application/json'
                 },
-                credentials: 'include'  // Thêm dòng này để gửi cookie cùng request
+                credentials: 'include'
             });
             return await response.json();
         } catch (error) {
             console.error('Error fetching data:', error);
         }
     }
-    
+
     async create(data) {
         try {
             const response = await fetch(`${this.baseUrl}/${this.path}`, {
@@ -59,7 +38,7 @@ class RestClient {
                 headers: {
                     'Content-Type': 'application/json'
                 },
-                credentials: 'include',  // Thêm dòng này để gửi cookie cùng request
+                credentials: 'include',
                 body: JSON.stringify(data)
             });
             return await response.json();
@@ -67,6 +46,21 @@ class RestClient {
             console.error('Error creating data:', error);
         }
     }
-    
+
+    async patch(data) {
+        try {
+            const response = await fetch(`${this.baseUrl}/${this.path}`, {
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                credentials: 'include',
+                body: JSON.stringify(data)
+            });
+            return await response.json();
+        } catch (error) {
+            console.error('Error updating data:', error);
+        }
+    }
 }
 export default RestClient;
